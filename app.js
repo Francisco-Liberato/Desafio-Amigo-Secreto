ee3c3c2adeee3c4c21dfeb5fe8c85fff43bd15ce
Francisco <franciscoliberatojr@hotmail.com>
2025-02-04 17:33:21 -0300

    Sorteio, mostrar lista e verificar se tem nome na lista

--- app.js
+++ app.js
@@ -2,18 +2,43 @@
 
 let amigos = [];
 
+let listaNomes = document.getElementById('listaAmigos');
+
 function adicionarAmigo() {
     if (document.getElementById('amigo').value === '') {
-        alert('Por favor, insira um nome.');
+        alert('Por favor, digite um nome.');
     } else {
     let amigo = document.getElementById('amigo').value;
     amigos.push(amigo);
     console.log(amigos);
     limpar();
+    mostrarAmigos();
 } 
 }
 
 function limpar () {
     let teste = document.querySelector('input');
     teste.value = '';
+}
+
+function mostrarAmigos() {
+    let ul = document.getElementById('listaAmigos');
+    ul.innerHTML = '';
+
+        amigos.forEach(function(amigo) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(amigo));
+        ul.appendChild(li);
+    });
+}
+
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Por favor, digite um nome.');
+} else {
+    let nomeSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = 'O amigo sorteado é: ' + nomeSorteado;
+}
 }
